414fa45d49f8d334742f35c772117cb1699e350a
Daniel Lee <dan.limerick@gmail.com>
2016-11-01 23:39:27 +0100

    fix plugin download script

--- get_plugins.sh
+++ get_plugins.sh
@@ -10,5 +10,5 @@ mkdir -p plugins/
 cd plugins
 
 for p in collector-cpu collector-docker collector-interface collector-iostat collector-load collector-meminfo publisher-graphite; do
-  curl -sfL https://s3-us-west-2.amazonaws.com/snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-cpu/latest/linux/x86_64/snap-plugin-$p -o snap-plugin-$p
+  curl -sfL https://s3-us-west-2.amazonaws.com/snap.ci.snap-telemetry.io/plugins/snap-plugin-$p/latest/linux/x86_64/snap-plugin-$p -o snap-plugin-$p
 done
